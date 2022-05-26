d929c5fc4a76240cb3fc539502c2cf5ae47e7de1
mukeshshah233 <88086896+mukeshshah233@users.noreply.github.com>
2022-05-26 22:50:36 +0530

    First cut checkin

---
+++ src/main/java/com/example/fapp/controller/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.fapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web1")
+public class DemoApplication {
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello web1 %s!", name);
+	}
+
+	/*
+	 * @GetMapping("/hello") public String hello() { return
+	 * String.format("Hello web1" ); }
+	 */
+}
